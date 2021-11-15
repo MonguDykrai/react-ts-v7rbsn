515972eb41f2e59c54c37d512c9c94fcce92d4e1
MonguDykrai <kisaihou@gmail.com>
2021-11-15 09:09:15 +0800

    Initial commit

---
+++ User.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useRequest } from '@umijs/hooks';
+
+function getUserInfo(id: number) {
+  return {
+    url: 'https://jsonplaceholder.typicode.com/users',
+    method: 'GET',
+    params: {
+      id,
+    },
+  };
+}
+
+const User: React.FC = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const { run, loading } = useRequest(getUserInfo, {
+    manual: true,
+    // pollingInterval: 3000,
+    onSuccess: (result, params) => {
+      console.log(result, params);
+      const [userInfo] = result;
+      setUserInfo(userInfo);
+    },
+  });
+  useEffect(() => {
+    run(5, false, 'hello world');
+  }, []);
+  if (loading) return <div>loading...</div>;
+  return (
+    <div>
+      <pre>{JSON.stringify(userInfo, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default User;
